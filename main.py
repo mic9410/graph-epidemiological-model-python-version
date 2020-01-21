b4a29735b1ff6c32f9cbeeedfd22a7cbf8cc847b
mic9410 <michal.foryt@gmail.com>
2020-01-21 20:19:19 +0100

    Implemented class Infection

--- main.py
+++ main.py
@@ -5,9 +5,24 @@ import matplotlib.pyplot as plt
 n_nodes = 20
 n_edges = 15
 rew_prob = 0.15  # prawdopodobieństwo zarażenia
-per_infected = 0.4  # Liczba zarażonych na osobę
+per_infected = 0.2  # Liczba zarażonych na osobę
 infection_duration = 7  # Czas trwania infekcji
 per_vaccinated = 0.3  # Liczba osób zaszczepionych
+
+
+class Infection:
+    def __init__(self, graph):
+        self.graph = graph
+
+    def infect(self, person_id):
+        self.graph.nodes[person_id].update({"infected": True})
+        self.graph.nodes[person_id].update({"color": 1})
+
+    def plot_graph_to_file(graph, filename="graph"):
+        nx.draw(graph)
+        plt.tight_layout()
+        plt.show()
+        plt.savefig(filename + ".png", format="PNG")
 
 
 def generate_social_network(n_nodes, n_edges, rew_prob):
@@ -18,20 +33,20 @@ def generate_social_network(n_nodes, n_edges, rew_prob):
         graph.nodes[i].update({"inf_days": 0})
         graph.nodes[i].update({"color": 0})
 
-    plot_graph_to_file(graph, "init")
-
+    # plot_graph_to_file(graph, "init")
     return graph
 
 
-def plot_graph_to_file(graph, filename="graph"):
-    nx.draw(graph)
-    plt.tight_layout()
-    plt.show()
-    plt.savefig(filename + ".png", format="PNG")
-
-
 def initialize(n_nodes, n_edges, rew_prob, per_infected):
-    network = generate_social_network(n_nodes, n_edges, rew_prob)
+    graph = generate_social_network(n_nodes, n_edges, rew_prob)
+    infection = Infection(graph)
+    for i in range(0, n_nodes):
+        if graph.nodes.get(i).get("exposed_to_infection") >= 1.0:
+            infection.infect(i)
+
+    infected = np.random.choice(graph.nodes, int(len(graph.nodes)*per_infected), replace=False)
+    for i in infected:
+        infection.infect(i)
 
 
 def run_simulation():
