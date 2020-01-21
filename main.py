3ecd65d4979ae917566805b7b407acdde24b7485
mic9410 <michal.foryt@gmail.com>
2020-01-21 18:55:47 +0100

    Implemented generate_social_network function

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import numpy as np
 from networkx import watts_strogatz_graph
 
-##  Parametry  ##
-dist = np.random.normal(0.5, 0.25)  # mi - średnia, sigma - odchylenie standardowe, a więc oczekujemy wartości między 0.25 a 0.75
 n_nodes = 20
 n_edges = 15
 rew_prob = 0.15  # prawdopodobieństwo zarażenia
@@ -11,18 +9,23 @@ infection_duration = 7  # Czas trwania infekcji
 per_vaccinated = 0.3  # Liczba osób zaszczepionych
 
 
-def generate_social_network(n_nodes, n_edges, rew_prob, dist):
+def generate_social_network(n_nodes, n_edges, rew_prob):
     graph = watts_strogatz_graph(n_nodes, n_edges, rew_prob)
-    
-    print(graph)
+    for i in range(0, n_nodes):
+        graph.nodes[i].update({"exposed_to_infection": np.random.normal(0.5, 0.25)})
+        graph.nodes[i].update({"infected": False})
+        graph.nodes[i].update({"inf_days": 0})
+        graph.nodes[i].update({"color": 0})
+
+    return graph
 
 
-def initialize(n_nodes, n_edges, rew_prob, dist, per_infected):
-    network = generate_social_network(n_nodes, n_edges, rew_prob, dist)
+def initialize(n_nodes, n_edges, rew_prob, per_infected):
+    network = generate_social_network(n_nodes, n_edges, rew_prob)
 
 
 def run_simulation():
-    network = initialize(n_nodes, n_edges, rew_prob, dist, per_infected)
+    network = initialize(n_nodes, n_edges, rew_prob, per_infected)
 
 
 run_simulation()
