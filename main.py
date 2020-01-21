405de7afb49f9af3a205f294cb9b8844d8e9390d
mic9410 <michal.foryt@gmail.com>
2020-01-21 19:22:43 +0100

    Implemented plot graph to file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import networkx as nx
 import numpy as np
-from networkx import watts_strogatz_graph
+import matplotlib.pyplot as plt
 
 n_nodes = 20
 n_edges = 15
@@ -10,14 +11,23 @@ per_vaccinated = 0.3  # Liczba osób zaszczepionych
 
 
 def generate_social_network(n_nodes, n_edges, rew_prob):
-    graph = watts_strogatz_graph(n_nodes, n_edges, rew_prob)
+    graph = nx.watts_strogatz_graph(n_nodes, n_edges, rew_prob)
     for i in range(0, n_nodes):
         graph.nodes[i].update({"exposed_to_infection": np.random.normal(0.5, 0.25)})
         graph.nodes[i].update({"infected": False})
         graph.nodes[i].update({"inf_days": 0})
         graph.nodes[i].update({"color": 0})
 
+    plot_graph_to_file(graph, "init")
+
     return graph
+
+
+def plot_graph_to_file(graph, filename="graph"):
+    nx.draw(graph)
+    plt.tight_layout()
+    plt.show()
+    plt.savefig(filename + ".png", format="PNG")
 
 
 def initialize(n_nodes, n_edges, rew_prob, per_infected):
